57d01b1b266a1fa50c86df08dd46cabb6fd22747
Difrizewee <rakhmanov.mar@gmail.com>
2026-02-07 02:09:08 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 const repo = "NEW-GRECHKO";
-
-// На GitHub Pages сайт живёт не в корне домена, а в /REPO
 const isGithubPages = process.env.GITHUB_PAGES === "true";
 const basePath = isGithubPages ? `/${repo}` : "";
 
 const nextConfig = {
-  output: "export",              // статический экспорт (out/)
-  trailingSlash: true,           // чтобы /NEW-GRECHKO/ работал как папка
+  output: "export",
+  trailingSlash: true,
   basePath,
   assetPrefix: basePath,
-  images: { unoptimized: true }, // next/image не работает на Pages без оптимизатора
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
